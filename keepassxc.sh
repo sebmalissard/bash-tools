30ca47572401b3fed56416ca461d5df69ad66264
Sebastien Malissard <sebastien.malissard@dbmail.com>
2022-11-16 22:52:49 +0100

    keepassxc: Add API to get username and password

--- keepassxc.sh
+++ keepassxc.sh
@@ -6,19 +6,26 @@ source "${SCRIPT_DIR}/messages.sh"
 
 ### Internal variables ###
 
-KEEPASSXC_DATABASE_FILE=""
-KEEPASSXC_DATABASE_PASSWORD=""
-KEEPASSXC_FATAL_ERROR="false"
+_KEEPASSXC_DATABASE_FILE=""
+_KEEPASSXC_DATABASE_PASSWORD=""
+_KEEPASSXC_FATAL_ERROR="false"
 
 
 ### Internal functions ###
 
-keepassxc_error()
+_keepassxc_error()
 {
-    if [ "${KEEPASSXC_FATAL_ERROR}" == "true" ]; then
+    if [ "${_KEEPASSXC_FATAL_ERROR}" == "true" ]; then
         fatal "$*"
     else
         error "$*"
+    fi
+}
+
+_keepassxc_check_database_file_valid()
+{
+    if [ ! -f "${_KEEPASSXC_DATABASE_FILE}" ]; then
+        _keepassxc_error "Cannot found '${_KEEPASSXC_DATABASE_FILE}'"
     fi
 }
 
@@ -28,39 +35,55 @@ keepassxc_error()
 keepassxc_set_error_fatal()
 {
     # Expected argument value: 'true' or 'false'
-    KEEPASSXC_FATAL_ERROR="${1}"
+    _KEEPASSXC_FATAL_ERROR="${1}"
 }
 
-keepassxc_get_database_path_from_user()
+keepassxc_set_database_path()
 {
-    echo "What is the keepassxc database to get configuration secret?"
-    read -r -e -p "Keepassxc database: " KEEPASSXC_DATABASE_FILE
-    if [ ! -f "${KEEPASSXC_DATABASE_FILE}" ]; then
-        keepassxc_error "Cannot found '${KEEPASSXC_DATABASE_FILE}'"
-    fi
+    _KEEPASSXC_DATABASE_FILE="${1}"
+    _keepassxc_check_database_file_valid
 }
 
-keepassxc_get_password_from_user_with_verify()
+keepassxc_set_database_path_from_user()
+{
+    echo "What is the keepassxc database file to be used to get secrets?"
+    read -r -e -p "Keepassxc database: " _KEEPASSXC_DATABASE_FILE
+    _keepassxc_check_database_file_valid
+}
+
+keepassxc_set_password_from_user_with_verify()
 {
     echo "What is the keepassxc password database?"
-    read -r -s -p "Password :" KEEPASSXC_DATABASE_PASSWORD
+    read -r -s -p "Password :" _KEEPASSXC_DATABASE_PASSWORD
     echo
     echo "Verifying password..."
-    if echo "${KEEPASSXC_DATABASE_PASSWORD}" | keepassxc-cli ls -q "${KEEPASSXC_DATABASE_FILE}" > "/dev/null"; then
+    if echo "${_KEEPASSXC_DATABASE_PASSWORD}" | keepassxc-cli ls -q "${_KEEPASSXC_DATABASE_FILE}" > "/dev/null"; then
         echo "Success"
     else
-        keepassxc_error "Invalid password."
+        _keepassxc_error "Invalid password."
     fi
+}
+
+keepassxc_get_username()
+{
+    entry="${1}"
+    keepassxc_get_attribute "${entry}" "UserName"
+}
+
+keepassxc_get_password()
+{
+    entry="${1}"
+    keepassxc_get_attribute "${entry}" "Password"
 }
 
 keepassxc_get_attribute()
 {
     entry="${1}"
     attribute="${2}"
-    echo "${KEEPASSXC_DATABASE_PASSWORD}" | keepassxc-cli show -q -a "${attribute}" "${KEEPASSXC_DATABASE_FILE}" "${entry}"
+    echo "${_KEEPASSXC_DATABASE_PASSWORD}" | keepassxc-cli show -q -a "${attribute}" "${_KEEPASSXC_DATABASE_FILE}" "${entry}"
 }
 
 keepassxc_clear_password()
 {
-    KEEPASSXC_DATABASE_PASSWORD="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    _KEEPASSXC_DATABASE_PASSWORD="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
